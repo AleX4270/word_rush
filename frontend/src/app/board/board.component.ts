4f3a296012920758466bcd0dcac69774de77ff8b
AleX4270 <aleksr427@gmail.com>
2024-03-19 20:04:50 +0100

    added counter

--- frontend/src/app/board/board.component.ts
+++ frontend/src/app/board/board.component.ts
@@ -9,9 +9,14 @@ import { LettersData, WordData } from "../shared/types/word.types";
     styleUrl: './board.component.scss'
 })
 export class BoardComponent {
+    private intervalId!: number;
+
     public isGameStarted: boolean = false;
     public lettersData: LettersData = {} as LettersData;
     public wordData: WordData = {} as WordData;
+    public counter: number = 0;
+    public isWordDataLoading: boolean = false;
+
     constructor() {
         this.lettersData.letters = ['w', 'o', 'r', 'd'];
         this.wordData.word = 'word';
@@ -19,11 +24,34 @@ export class BoardComponent {
 
     @HostListener('window:keydown', ['$event'])
     public handleStartGame(event: KeyboardEvent) {
-        if(event.code === 'Space') {
+        if(event.code !== 'Space' || this.isWordDataLoading) return;
+
+        if(!this.isGameStarted) {
             console.log('game started!');
+            this.startCounter();
             this.isGameStarted = true;
+        }
+        else {
+            console.log('game stopped!');
+            this.stopCounter();
+            this.isGameStarted = false;
         }
     }
 
+    private getWordData(): void {
+        this.isWordDataLoading = true;
+        //TODO: Add logic to get word from API
+    }
 
+    public startCounter(): void {
+        this.counter = 0;
+        this.intervalId = setInterval(() => {
+            this.counter++;
+        }, 1000);
+    }
+
+    public stopCounter(): void {
+        clearInterval(this.intervalId);
+        this.getWordData();
+    }
 }
---
+++ frontend/src/app/shared/classes/stopwatch.ts
@@ -0,0 +1,3 @@
+export class Stopwatch {
+    private _startTime: number;
+}
