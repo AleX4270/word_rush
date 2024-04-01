e082c3a7d88b11a7d5c8ecc5f2d20af1591c97af
AleX4270 <aleksr427@gmail.com>
2024-04-01 19:56:34 +0200

    added app version and copyright info to the footer

--- frontend/src/app/shared/components/footer/footer.component.ts
+++ frontend/src/app/shared/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from "../../../../environments/environment";
 
 @Component({
   selector: 'app-footer',
@@ -8,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+    public appVersion: string;
+    public currentYear: number;
 
+    constructor() {
+        this.appVersion = environment.appVersion;
+        this.currentYear = new Date().getFullYear();
+    }
 }
